1b257721a130798cbaed585ebb1cb25542109d36
Aboubacar Sidig Diallo <diallo1194asd@gmail.com>
2021-01-09 21:59:14 +0100

    implementation du contenu de la class BoiteChocolat, nombreDeChocolat et PUB , puis la redefinition de prix()

--- Bicard/BoiteChocolat.java
+++ Bicard/BoiteChocolat.java
@@ -1,4 +1,12 @@
 package Bicard;
 
-public class BoiteChocolat {
+public class BoiteChocolat extends Produit{
+    private float nombreDeChocolat = 0;
+    private final float PUB = 0.5f;
+
+    @Override
+    public float prix() {
+        return nombreDeChocolat * PUB;
+    }
+
 }
