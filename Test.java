593a8d0e07e72ba8f0467142951ca16363a01df0
Aboubacar Sidig Diallo <diallo1194asd@gmail.com>
2021-01-09 20:57:58 +0100

    Implementation d'une class abstraite Produit

--- Test.java
+++ Test.java
@@ -7,9 +7,14 @@ public class Test {
     public static int HEURE = 0 ;
 
     public static void main(String[] args) {
+
         BoiteChocolat b1 = new BoiteChocolat(24);
         Dinde d1 = new Dinde(8.250f);
         Saumon s1 = new Saumon(Saumon.Origine.ALASKA, 3.100f);
+
+
+
+
 
     }
 }
